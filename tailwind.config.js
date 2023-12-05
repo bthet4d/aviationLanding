705e206e2075948e4de7690d5768c0d48dfbf759
Bryan Thetford <bryan.thetford@gmail.com>
2023-12-05 10:12:26 -0600

    basic structure with placeholder images

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#02061E',	/* Main Primary color */
+        'secondary': '#060C34',
+      },
+      backgroundImage: {
+        'bg-hero-pattern': 'url("src/images/classicPlaneTakingOff.png")',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
